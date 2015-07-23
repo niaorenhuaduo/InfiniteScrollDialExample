6a95117e7f22b4ddebbb247e5a3618478e70712f
sugar <sugar@imac.local>
2015-07-23 20:54:08 +0300

    Initial Commit

---
+++ InfiniteScrollDialExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InfiniteScrollDialExample
+//
+//  Created by sugar on 23/7/15.
+//  Copyright (c) 2015 Konstantinos Hondros. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ InfiniteScrollDialExampleTests/InfiniteScrollDialExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  InfiniteScrollDialExampleTests.swift
+//  InfiniteScrollDialExampleTests
+//
+//  Created by sugar on 23/7/15.
+//  Copyright (c) 2015 Konstantinos Hondros. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class InfiniteScrollDialExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
